a146397f6947c7f05a9535d6e74796146923cd52
Stefan Trauth <mail@stefantrauth.de>
2015-10-13 16:13:59 +0200

    object oriented approach. more methods to have nicer and cleaner code

--- lib/podcast-to-youtube.rb
+++ lib/podcast-to-youtube.rb
@@ -7,28 +7,65 @@ require 'rubygems'
 require 'json'
 
 class PodcastUploader
-	def self.upload(podcast_feed_url, client_secret_file_path = 'client_secret.json', video_category_id = '28')
-		
+
+	@client_secret
+	@client_secret_file_path
+	@account
+
+	def initialize(client_secret_file_path = 'client_secret.json')
+		load_configuration client_secret_file_path
+		authenticate_youtube
+	end
+
+	def load_configuration(path)
 		if File.file?(client_secret_file_path)
-			client_secret = JSON.parse(File.read(client_secret_file_path))
+			@client_secret = JSON.parse(File.read(client_secret_file_path))
+			@client_secret_file_path = path
 		else 
 			raise "Please provide client_secret.json. This is required for the Youtube API authentication. More information can be found in the Readme."
 		end
+	end
 
-		puts "parsing feed"
-		feed = Feedjira::Feed.fetch_and_parse podcast_feed_url
+	def save_configuration
+		@client_secret['installed']['refresh_token'] = @account.authentication.refresh_token
+		File.write(client_secret_file_path, @client_secret.to_json)
+	end
 
+	def authenticate_youtube
 		puts "connecting to youtube account"
-		Yt.configure do |config|
-		  config.client_id = client_secret['installed']['client_id']
-		  config.client_secret = client_secret['installed']['client_secret']
+		# check for refresh token in config file
+		if !@client_secret['installed']['refresh_token'].empty?
+			puts "using refresh token"
+			authenticate_youtube_by_refresh_token
+		else
+			# otherwise authenticate with oauth2
+			Yt.configure do |config|
+				config.client_id = @client_secret['installed']['client_id']
+				config.client_secret = @client_secret['installed']['client_secret']
+			end
+			redirect_uri = 'urn:ietf:wg:oauth:2.0:oob' # special redirect uri to make the user copy the auth_code to the application
+			puts "open this url in a browser"
+			puts Yt::Account.new(scopes: ['youtube'], redirect_uri: redirect_uri).authentication_url
+			puts "paste the authentication code here and press enter"
+			auth_code = gets.chomp
+			@account = Yt::Account.new authorization_code: auth_code, redirect_uri: redirect_uri
+
+			save_configuration
 		end
-		redirect_uri = 'urn:ietf:wg:oauth:2.0:oob' # special redirect uri to make the user copy the auth_code to the application
-		puts "open this url in a browser"
-		puts Yt::Account.new(scopes: ['youtube'], redirect_uri: redirect_uri).authentication_url
-		puts "paste the authentication code here and press enter"
-		auth_code = gets.chomp
-		account = Yt::Account.new authorization_code: auth_code, redirect_uri: redirect_uri
+	end
+
+	def authenticate_youtube_by_refresh_token
+		@account = Yt::Account.new refresh_token: @client_secret['installed']['refresh_token']
+	end
+
+
+	def parse_feed
+		puts "parsing feed"
+		return Feedjira::Feed.fetch_and_parse podcast_feed_url
+	end
+
+	def upload(podcast_feed_url, video_category_id = '28')
+		feed = parse_feed
 
 		feed.entries.reverse_each do |entry|
 
@@ -58,15 +95,16 @@ class PodcastUploader
 					# upload to youtube
 					video_description = "#{entry.itunes_summary}\n\nMehr Infos und Links zur Episode: #{entry.url}\nVeröffentlicht: #{entry.published}\nEpisode herunterladen (Audio): #{entry.url}\n\nPodcast Webseite: #{feed.url}\nPodcast Abonnieren: #{feed.url}\nPodcast Author: #{feed.itunes_author}"
 					video_title = "#{feed.title} - #{entry.title}"
-					if account.videos.any? {|video| video.title == video_title }
+					if @account.videos.any? {|video| video.title == video_title }
 						puts "do not upload video, as it is already online"
 					else
 						puts "uploading videofile to Youtube"
 						# refresh authentication if expired
-						if account.authentication.expired?
-							account = Yt::Account.new refresh_token: account.authentication.refresh_token
+						if @account.authentication.expired?
+							authenticate_youtube_by_refresh_token
+							save_configuration
 						end
-						account.upload_video videofile, privacy_status: :private, title: video_title, description: video_description, category_id: video_category_id, tags: %w(podcast)
+						@account.upload_video videofile, privacy_status: :private, title: video_title, description: video_description, category_id: video_category_id, tags: %w(podcast)
 					end
 				else
 					raise "generating videofile #{videofile} failed"
@@ -75,6 +113,8 @@ class PodcastUploader
 				raise "downloading audiofile or coverart failed"
 			end	
 		end
-
 	end
+
+	private :load_configuration :save_configuration :authenticate_youtube :authenticate_youtube_by_refresh_token :parse_feed
+
 end
